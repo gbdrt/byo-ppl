679396786a9cfbfae9bf6c21aa2d895d893d81e8
Guillaume Baudart <guillaume.baudart@inria.fr>
2021-12-17 15:00:42 +0100

    Cleanup

--- byoppl/basic.ml
+++ byoppl/basic.ml
@@ -1,11 +1,13 @@
 module Rejection_sampling = struct
   type prob = Prob
-  let sample _ d = Distribution.draw d
+
+  let sample _prob d = Distribution.draw d
 
   exception Reject
-  let assume _ p = if not p then raise Reject
 
-  let observe prob d x = 
+  let assume _prob p = if not p then raise Reject
+
+  let observe prob d x =
     let y = Distribution.draw d in
     assume prob (x = y)
 
@@ -16,16 +18,15 @@ module Rejection_sampling = struct
 end
 
 module Importance_sampling = struct
-  type prob = {id: int; scores: float array}
+  type prob = { id : int; scores : float array }
 
-  let sample _ d = Distribution.draw d
+  let sample _prob d = Distribution.draw d
   let factor prob s = prob.scores.(prob.id) <- prob.scores.(prob.id) +. s
-
   let observe prob d x = factor prob (Distribution.logpdf d x)
   let assume prob p = factor prob (if p then 0. else -.infinity)
 
-  let infer ?(n=1000) model data = 
+  let infer ?(n = 1000) model data =
     let scores = Array.make n 0. in
-    let values = Array.mapi (fun i _ -> model {id = i; scores} data) scores in
+    let values = Array.mapi (fun i _ -> model { id = i; scores } data) scores in
     Distribution.support ~values ~logits:scores
 end
